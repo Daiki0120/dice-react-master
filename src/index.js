cd91ce87a5663aee1437f7bc963fa2852e4958d4
Daiki0120 <dusui2017@kellogg.northwestern.edu>
2016-12-09 17:23:48 -0600

    f

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ var ReactDOM = require('react-dom')
 var getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var Die = React.createClass({
+render: function() {
+   return (
+     <div className="col-sm-4">
+       <img src={"http://golearntocode.com/images/dice/" + this.props.number +".png"} />
+     </div>
+   )
+ }
+})
 
 var App = React.createClass({
   render: function() {
@@ -12,12 +21,8 @@ var App = React.createClass({
       <div className="container">
         <h1>Welcome to the KIEI-924 Casino!</h1>
         <div className="row">
-          <div className="col-sm-4">
-            <img src={"http://golearntocode.com/images/dice/3.png"} />
-          </div>
-          <div className="col-sm-4">
-            <img src={"http://golearntocode.com/images/dice/4.png"} />
-          </div>
+        <Die number={getRandomInt(1, 6)}/>
+        <Die number={getRandomInt(1, 6)}/>
         </div>
       </div>
     )
